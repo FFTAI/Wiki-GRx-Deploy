f79f6dcc8d7885872000ae9a37a0a650c5128ffe
chuanbo liu <chuanbo.liu@fftai.com>
2024-05-24 19:17:58 +0800

    Version: V 0.0.1.4
    Updates:
    1. Update the state estimator
    2. Alter the IMU coordinate to the same with the robot itself

--- StateEstimator/include/StateData.h
+++ StateEstimator/include/StateData.h
@@ -6,65 +6,67 @@
 
 class StateData
 {
-    public: 
-        // State estimator input data
-        Eigen::VectorXd imu_data; 
-        Eigen::VectorXd imu_data_init; 
-        int imu_init; 
-        Eigen::VectorXd q_motor; 
-        Eigen::VectorXd qd_motor; 
-        Eigen::VectorXd qdd_motor; 
-        Eigen::VectorXd tau_motor;
+public:
+    // State estimator input data
+    Eigen::VectorXd imu_data;
+    Eigen::VectorXd imu_data_init;
+    int imu_init;
+    Eigen::VectorXd q_motor;
+    Eigen::VectorXd qd_motor;
+    Eigen::VectorXd qdd_motor;
+    Eigen::VectorXd tau_motor;
 
-        // State Update data 
-        RigidBodyDynamics::Model * robot_model; 
-        Eigen::Vector3d body_T_offset; 
-        Eigen::Vector3d left_foot_T_offset; 
-        Eigen::Vector3d right_foot_T_offset; 
+    // State Update data
+    RigidBodyDynamics::Model *robot_model;
+    Eigen::Vector3d body_T_offset;
+    Eigen::Vector3d left_foot_T_offset;
+    Eigen::Vector3d right_foot_T_offset;
 
-        // Model data
-        std::vector<uint> id_body;
-        std::vector<RigidBodyDynamics::Body *> robot_body;
-        std::vector<RigidBodyDynamics::Joint *> robot_joint;
-        Eigen::Matrix<double, Eigen::Dynamic, 1> q_lbound;
-        Eigen::Matrix<double, Eigen::Dynamic, 1> q_ubound;
-        Eigen::Matrix<double, Eigen::Dynamic, 1> qd_bound;
-        Eigen::Matrix<double, Eigen::Dynamic, 1> tau_bound;
-        Eigen::Vector3d gravity; 
-        int ndof = 0;
+    // Model data
+    std::vector<uint> id_body;
+    std::vector<RigidBodyDynamics::Body *> robot_body;
+    std::vector<RigidBodyDynamics::Joint *> robot_joint;
+    Eigen::Matrix<double, Eigen::Dynamic, 1> q_lbound;
+    Eigen::Matrix<double, Eigen::Dynamic, 1> q_ubound;
+    Eigen::Matrix<double, Eigen::Dynamic, 1> qd_bound;
+    Eigen::Matrix<double, Eigen::Dynamic, 1> tau_bound;
+    Eigen::Vector3d gravity;
+    int ndof = 0;
 
-        // Middle State data
-        Eigen::VectorXd grf; 
-        Eigen::VectorXd grf_pre; 
-        int touch_index; 
-        int touch_index_pre; 
-        int foot_type; 
-        double grf_lb{100}; 
-        double grf_ub{0.8 * 480};
+    // Middle State data
+    Eigen::VectorXd grf;
+    Eigen::VectorXd grf_pre;
+    int touch_index;
+    int touch_index_pre;
+    int foot_type;
+    double grf_lb{110};
+    double grf_ub{0.8 * 480};
+    double timer;
+    double dt;
 
-        Eigen::MatrixXd body_jaccobi; 
-        Eigen::MatrixXd left_foot_jaccobi; 
-        Eigen::MatrixXd right_foot_jaccobi; 
-        Eigen::MatrixXd body_jaccobi_dot;
-        Eigen::MatrixXd left_foot_jaccobi_dot;
-        Eigen::MatrixXd right_foot_jaccobi_dot;
+    Eigen::MatrixXd body_jaccobi;
+    Eigen::MatrixXd left_foot_jaccobi;
+    Eigen::MatrixXd right_foot_jaccobi;
+    Eigen::MatrixXd body_jaccobi_dot;
+    Eigen::MatrixXd left_foot_jaccobi_dot;
+    Eigen::MatrixXd right_foot_jaccobi_dot;
 
-        // Torso Position
-        Eigen::Vector3d posTorso; 
-        Eigen::Vector3d velTorso; 
-        Eigen::Vector3d velTorso_filt; 
+    // Torso Position
+    Eigen::Vector3d posTorso;
+    Eigen::Vector3d velTorso;
+    Eigen::Vector3d velTorso_last;
+    Eigen::Vector3d velTorso_filt;
 
-        int n_dof;
-        int dim; 
+    int n_dof;
+    int dim;
 
-        // State estimator output data
-        Eigen::MatrixXd x_a_body; 
-        Eigen::MatrixXd x_a_left_foot; 
-        Eigen::MatrixXd x_a_right_foot; 
+    // State estimator output data
+    Eigen::MatrixXd x_a_body;
+    Eigen::MatrixXd x_a_left_foot;
+    Eigen::MatrixXd x_a_right_foot;
 
-        StateData();
-        ~StateData(); 
-
-}; 
+    StateData();
+    ~StateData();
+};
 
 #endif // STATEDATA_H
--- StateEstimator/include/StateEstimator.h
+++ StateEstimator/include/StateEstimator.h
@@ -7,32 +7,29 @@
 #include "Basics.h"
 #include "VelKalmanFilter.h"
 
-#include <QJsonArray>
-#include <QJsonDocument>
-#include <QJsonObject>
-#include <QJsonParseError>
-#include <QJsonValue>
-#include <QString>
+#include <fstream>
+#include <nlohmann/json.hpp>
+
+#include "urdfreader.h"
 
 /**
  * @brief The Estimator class represents a state estimator for a robot.
- * 
+ *
  * This class is responsible for estimating the state of a robot based on the provided sensor data.
  * It uses the RBDL library for robot dynamics calculations and provides methods to initialize the estimator,
  * run the estimation process, and retrieve the estimated state.
  */
 class Estimator
 {
-public: 
+public:
     Estimator();
-    ~Estimator(); 
+    ~Estimator();
 
     /**
      * @brief Initializes the estimator with the robot model.
      * @param model_path The path to the robot model file.
-     * @param dt The time step for the state estimation process.
      */
-    void init(QString model_path, double dt); 
+    bool init(std::string model_path, double dt);
 
     /**
      * @brief Runs the state estimation process.
@@ -41,7 +38,7 @@ public:
      * @param qdd_a The joint accelerations.
      * @param tau_a The joint torques.
      */
-    void run(Eigen::VectorXd q_a, Eigen::VectorXd qd_a, Eigen::VectorXd qdd_a, Eigen::VectorXd tau_a, Eigen::VectorXd imu_data); 
+    void run(Eigen::VectorXd q_a, Eigen::VectorXd qd_a, Eigen::VectorXd qdd_a, Eigen::VectorXd tau_a, Eigen::VectorXd imu_data);
 
     /**
      * @brief Retrieves the estimated state.
@@ -49,40 +46,62 @@ public:
      */
     Eigen::MatrixXd get_state();
 
-private: 
+    /**
+     * @brief Retrieves the estimated state.
+     * @return The estimated body state as a vector.
+     */
+    Eigen::VectorXd get_base_state();
+
+private:
     /**
      * @brief Updates the internal state of the estimator.
      */
-    void state_update(); 
+    void state_update();
 
     /**
      * @brief Constructs the robot model based on the provided model file.
      * @param model_path The path to the robot model file.
      */
-    void robot_constructor(QString model_path); 
+    bool robot_constructor(std::string model_path);
 
     /**
      * @brief Estimates the ground reaction forces.
      * @return True if the GRF estimation is successful, false otherwise.
      */
-    bool grf_estimator(); 
+    bool grf_estimator();
 
     /**
      * @brief State machine that estimates the state of stepping
      * @return True if it is successful, false otherwise.
      */
-    bool step_state_machine(); 
+    bool step_state_machine();
 
     /**
      * @brief Estimates the velocity of the floating base.
      * @return True if it is successful, false otherwise.
      */
-    bool floating_base_estimator(); 
+    bool floating_base_estimator();
 
-    VelKalmanFilter * accKalman; 
-    StateData * state; ///< Pointer to the state data.
-    Eigen::MatrixXd states; ///< Matrix to store and the estimated result states.
+    /**
+     * @brief Converts an axis of type string to the corresponding int value.
+     * @param axis The axis value of type string read from the json.
+     */
+    int axis_from_string(const std::string &axis);
 
-}; 
+    /**
+     * @brief Read the value of the corresponding key in the json file and
+     *        save it in the input parameter vars.
+     * @param j json file data.
+     * @param key The field to look for.
+     * @param vars The value corresponding to the field.
+     */
+    template <typename... T>
+    void getValue(const nlohmann::json &j, const std::string &key, T &...vars);
+
+    VelKalmanFilter *accKalman;
+    StateData *state;           ///< Pointer to the state data.
+    Eigen::MatrixXd states;     ///< Matrix to store and the estimated result states.
+    Eigen::VectorXd base_state; ///< Vector to store the estimated base state result
+};
 
 #endif // STATEESTIMATOR_H
