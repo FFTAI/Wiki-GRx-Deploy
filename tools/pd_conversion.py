2e9f3c5d7356340f72e924b28fe26ee4116ff987
chenxin199305@163.com <chenxin199305@163.com>
2024-06-17 18:17:23 +0800

    Update pd_conversion.py

--- tools/pd_conversion.py
+++ tools/pd_conversion.py
@@ -34,66 +34,6 @@ def GR1T1_pd_dict() -> dict:
     return pd_dict
 
 
-def GR1T1V2_pd_dict() -> dict:
-    pd_dict = {
-        "l_hip_roll": [FSAType.FSA_TYPE_802029, 251.625, 14.72],
-        "l_hip_yaw": [FSAType.FSA_TYPE_601750, 362.52, 10.0833],
-        "l_hip_pitch": [FSAType.FSA_TYPE_1307E, 200, 11],
-        "l_knee_pitch": [FSAType.FSA_TYPE_1307E, 200, 11],
-        "l_ankle_pitch": [FSAType.FSA_TYPE_36B36E, 10.98, 0.6],
-        "l_ankle_roll": [FSAType.FSA_TYPE_36B36E, 10.98, 0.6],
-        "r_hip_roll": [FSAType.FSA_TYPE_802029, 251.625, 14.72],
-        "r_hip_yaw": [FSAType.FSA_TYPE_601750, 362.52, 10.0833],
-        "r_hip_pitch": [FSAType.FSA_TYPE_1307E, 200, 11],
-        "r_knee_pitch": [FSAType.FSA_TYPE_1307E, 200, 11],
-        "r_ankle_pitch": [FSAType.FSA_TYPE_36B36E, 10.98, 0.6],
-        "r_ankle_roll": [FSAType.FSA_TYPE_36B36E, 10.98, 0.6],
-        "waist_yaw": [FSAType.FSA_TYPE_601750, 362.52, 10.0833],
-        "waist_pitch": [FSAType.FSA_TYPE_601750, 362.52, 10.0833],
-        "waist_roll": [FSAType.FSA_TYPE_601750, 362.52, 10.0833],
-        "l_shoulder_pitch": [FSAType.FSA_TYPE_361480, 92.85, 2.575],
-        "l_shoulder_roll": [FSAType.FSA_TYPE_361480, 92.85, 2.575],
-        "l_shoulder_yaw": [FSAType.FSA_TYPE_3611100, 112.06, 3.1],
-        "l_elbow_pitch": [FSAType.FSA_TYPE_3611100, 112.06, 3.1],
-        "r_shoulder_pitch": [FSAType.FSA_TYPE_361480, 92.85, 2.575],
-        "r_shoulder_roll": [FSAType.FSA_TYPE_361480, 92.85, 2.575],
-        "r_shoulder_yaw": [FSAType.FSA_TYPE_3611100, 112.06, 3.1],
-        "r_elbow_pitch": [FSAType.FSA_TYPE_3611100, 112.06, 3.1],
-    }
-
-    return pd_dict
-
-
-def GR1T1V3_pd_dict() -> dict:
-    pd_dict = {
-        "l_hip_roll": [FSAType.FSA_TYPE_802029, 251.625, 14.72],
-        "l_hip_yaw": [FSAType.FSA_TYPE_601750, 362.52, 10.0833],
-        "l_hip_pitch": [FSAType.FSA_TYPE_13014E, 200, 11],
-        "l_knee_pitch": [FSAType.FSA_TYPE_13014E, 200, 11],
-        "l_ankle_pitch": [FSAType.FSA_TYPE_36B36E, 10.98, 0.6],
-        "l_ankle_roll": [FSAType.FSA_TYPE_36B36E, 10.98, 0.6],
-        "r_hip_roll": [FSAType.FSA_TYPE_802029, 251.625, 14.72],
-        "r_hip_yaw": [FSAType.FSA_TYPE_601750, 362.52, 10.0833],
-        "r_hip_pitch": [FSAType.FSA_TYPE_13014E, 200, 11],
-        "r_knee_pitch": [FSAType.FSA_TYPE_13014E, 200, 11],
-        "r_ankle_pitch": [FSAType.FSA_TYPE_36B36E, 10.98, 0.6],
-        "r_ankle_roll": [FSAType.FSA_TYPE_36B36E, 10.98, 0.6],
-        "waist_yaw": [FSAType.FSA_TYPE_601750, 362.52, 10.0833],
-        "waist_pitch": [FSAType.FSA_TYPE_601750, 362.52, 10.0833],
-        "waist_roll": [FSAType.FSA_TYPE_601750, 362.52, 10.0833],
-        "l_shoulder_pitch": [FSAType.FSA_TYPE_361480, 92.85, 2.575],
-        "l_shoulder_roll": [FSAType.FSA_TYPE_361480, 92.85, 2.575],
-        "l_shoulder_yaw": [FSAType.FSA_TYPE_3611100, 112.06, 3.1],
-        "l_elbow_pitch": [FSAType.FSA_TYPE_3611100, 112.06, 3.1],
-        "r_shoulder_pitch": [FSAType.FSA_TYPE_361480, 92.85, 2.575],
-        "r_shoulder_roll": [FSAType.FSA_TYPE_361480, 92.85, 2.575],
-        "r_shoulder_yaw": [FSAType.FSA_TYPE_3611100, 112.06, 3.1],
-        "r_elbow_pitch": [FSAType.FSA_TYPE_3611100, 112.06, 3.1],
-    }
-
-    return pd_dict
-
-
 def GR1T2_pd_dict() -> dict:
     pd_dict = {
         "l_hip_roll": [FSAType.FSA_TYPE_802030, 251.625, 14.72],
